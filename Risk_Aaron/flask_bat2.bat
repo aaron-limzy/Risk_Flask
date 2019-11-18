501579644ca241738f021cd24db38ef5614dec01
Aaron Lim <aaron.limzy@uq.net.au>
2019-11-18 12:21:54 +0800

    Update flask_bat2.bat

--- Risk_Aaron/flask_bat2.bat
+++ Risk_Aaron/flask_bat2.bat
@@ -1,3 +1,4 @@
-cd C:\Users\Administrator\Desktop\Risk\Aaron\Risk_Flask\Risk_Aaron
-venv/bin/activate
-flask run --port 5001 --host 0.0.0.0
+cd C:\Users\Administrator\Desktop\Risk\Aaron\Risk_Flask
+venv\Scripts\activate
+cd Risk_Aaron
+flask run --port 5000 --host 0.0.0.0
