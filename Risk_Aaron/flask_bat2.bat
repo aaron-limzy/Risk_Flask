93499b14b9f761128bc0d8923e5896c04eaf988d
aaron-limzy <51228350+aaron-limzy@users.noreply.github.com>
2019-11-18 12:21:45 +0800

    a

--- Risk_Aaron/flask_bat2.bat
+++ Risk_Aaron/flask_bat2.bat
@@ -1,2 +1,8 @@
+<<<<<<< Updated upstream
 cd C:\Users\Administrator\Desktop\Risk\Aaron\Risk_Flask\Risk_Aaron
+=======
+cd C:\Users\Administrator\Desktop\Risk\Aaron\Risk_Flask
+venv\Scripts\activate
+cd Risk_Aaron
+>>>>>>> Stashed changes
 flask run --port 5001 --host 0.0.0.0
