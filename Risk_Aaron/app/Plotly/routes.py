ed40ce77edb4ecd0edd1f8f13091382e9fa95af3
aaron-limzy <51228350+aaron-limzy@users.noreply.github.com>
2020-07-08 18:24:11 +0800

    Update routes.py

--- Risk_Aaron/app/Plotly/routes.py
+++ Risk_Aaron/app/Plotly/routes.py
@@ -1368,7 +1368,7 @@ def plot_account_details(account_details):
 
     # Use the hovertext kw argument for hover text
     fig = go.Figure(data=[go.Bar(x=x, y=y,
-                                 hovertext=['Deposit', 'Withdrawal', 'Profits'], text=y, textposition='outside')])
+                                 hovertext=['Deposit', 'Withdrawal', 'Profits'], text=y, textposition='auto')])
     # Customize aspect
     fig.update_traces(marker_color='rgb(158,202,225)', marker_line_color='rgb(8,48,107)',
                       marker_line_width=1.5, opacity=0.6)
