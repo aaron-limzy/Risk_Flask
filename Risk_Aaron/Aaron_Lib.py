c605b6e99cd59fb3ef5a74b2ccaf88b54f5fadbf
aaron-limzy <aaron.limzy@uq.net.au>
2021-07-29 18:37:08 +0800

    Update Aaron_Lib.py

--- Risk_Aaron/Aaron_Lib.py
+++ Risk_Aaron/Aaron_Lib.py
@@ -103,7 +103,7 @@ def Send_Email(To_recipients, cc_recipients, Subject, HTML_Text, Attachment_Name
     # server.sendmail(me, To_recipients + Bcc_recipients + cc_recipients, msg.as_string())
     return
 
-def create_email_virtual_file(txt)
+def create_email_virtual_file(txt):
 
     f = StringIO()
     f.write(txt)
