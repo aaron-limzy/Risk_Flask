b4541b753ba45dd50b2c1c77b35fb33b3e9b1e80
aaron-limzy <aaron.limzy@uq.net.au>
2022-02-25 20:27:38 +0800

    Update Helper_Flask_Lib.py

--- Risk_Aaron/Helper_Flask_Lib.py
+++ Risk_Aaron/Helper_Flask_Lib.py
@@ -888,7 +888,7 @@ def Calculate_Net_position(df_data):
     ret_val.drop(['LOT_PRICE'], axis=1, inplace=True)
     #
 
-    ret_val["LOTS"] = ret_val["LOTS"].apply(lambda x: "{:.3f}".format(x))  # Print in 2 D.P.
+    ret_val["LOTS"] = ret_val["LOTS"].apply(lambda x: "{:.2f}".format(x))  # Print in 2 D.P.
     ret_val = ret_val[["SYMBOL", "LOTS", "NET_LOTS", "AVG_PRICE", "PROFIT", "SWAPS"]] # Choose the column orientation
     print(ret_val)
     return ret_val
